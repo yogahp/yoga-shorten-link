6eae64957e53bafbe5d187ded64f049c542eb0cd
Yoga Hapriana <thenelse@rocketmail.com>
2018-01-07 17:26:11 +0700

    Change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,9 @@
 
 ActiveRecord::Schema.define(version: 20180105193025) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "shorten_links", force: :cascade do |t|
     t.string "url"
     t.string "slug"
