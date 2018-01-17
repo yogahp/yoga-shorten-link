cf34eb9380f76b61d95dfa9d92aebb4fc469d5ea
Yoga Hapriana <thenelse@rocketmail.com>
2018-01-17 11:48:24 +0700

    Fixing code complexity when create shorten link

--- app/controllers/shorten_links_controller.rb
+++ app/controllers/shorten_links_controller.rb
@@ -15,22 +15,7 @@ class ShortenLinksController < ApplicationController
   def create
     @shorten_link = ShortenLink.new(shorten_link_params)
 
-    respond_to do |format|
-      if @shorten_link.save
-        url_link = self.class.helpers.link_to(raw("<span class='fa fa-external-link'></span>"), "#{request.url}/#{@shorten_link.slug}", target: "_blank")
-        format.html { redirect_to(new_shorten_link_path, notice: "Shorten link was successfully created. Your shorten url is #{request.url}/#{@shorten_link.slug} #{url_link}", flash: { html_safe: true }) }
-      else
-        existing_shorten_link = ShortenLink.find_by(url: shorten_link_params[:url])
-
-        if existing_shorten_link.present?
-          url_link = self.class.helpers.link_to(raw("<span class='fa fa-external-link'></span>"), "#{request.url}/#{existing_shorten_link.slug}", target: "_blank")
-          flash.now[:notice] = "Shorten link was existed. Your shorten url is #{request.url}/#{existing_shorten_link.slug} #{url_link}".html_safe
-        end
-
-        format.html { render :new }
-        format.json { render json: @shorten_link.errors, status: :unprocessable_entity }
-      end
-    end
+    respond_to_format
   end
 
   def show
@@ -52,5 +37,24 @@ class ShortenLinksController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def shorten_link_params
       params.require(:shorten_link).permit(:url, :custom, :slug)
+    end
+
+    def respond_to_format
+      respond_to do |format|
+        if @shorten_link.save
+          url_link = self.class.helpers.link_to(raw("<span class='fa fa-external-link'></span>"), "#{request.url}/#{@shorten_link.slug}", target: "_blank")
+          format.html { redirect_to(new_shorten_link_path, notice: "Shorten link was successfully created. Your shorten url is #{request.url}/#{@shorten_link.slug} #{url_link}", flash: { html_safe: true }) }
+        else
+          existing_shorten_link = ShortenLink.find_by(url: shorten_link_params[:url])
+
+          if existing_shorten_link.present?
+            url_link = self.class.helpers.link_to(raw("<span class='fa fa-external-link'></span>"), "#{request.url}/#{existing_shorten_link.slug}", target: "_blank")
+            flash.now[:notice] = "Shorten link was existed. Your shorten url is #{request.url}/#{existing_shorten_link.slug} #{url_link}".html_safe
+          end
+
+          format.html { render :new }
+          format.json { render json: @shorten_link.errors, status: :unprocessable_entity }
+        end
+      end
     end
 end
